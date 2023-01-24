28476de413455caf1e96fafec37fb438b20ab3d2
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2023-01-24 19:20:53 +0100

    feat: import home component into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -27,6 +28,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   )
 }

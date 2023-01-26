f1191ea36d303f37df8beff64f59696e5c85db0a
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2023-01-26 08:47:27 +0100

    feat: edit benifit component

--- src/scenes/benefits/Benifit.tsx
+++ src/scenes/benefits/Benifit.tsx
@@ -1,11 +1,42 @@
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
 
-type Props = {}
+type Props = {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Benifit = (props: Props) => {
+const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div>Benifit</div>
-  )
-}
+    <motion.div
+      variants={childVariant}
+      className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+    >
+      <div className="mb-4 flex justify-center">
+        <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
+          {icon}
+        </div>
+      </div>
 
-export default Benifit
+      <h4 className="font-bold">{title}</h4>
+      <p className="my-3">{description}</p>
+      <AnchorLink
+        className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+        onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+        href={`#${SelectedPage.ContactUs}`}
+      >
+        <p>Learn More</p>
+      </AnchorLink>
+    </motion.div>
+  );
+};
+
+export default Benefit;

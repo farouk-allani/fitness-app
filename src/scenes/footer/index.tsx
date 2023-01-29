b1b527cf286948b4d934ac1740937d405d065b8b
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2023-01-29 13:07:58 +0100

    feat: edit footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -7,21 +7,23 @@ const Footer = () => {
         <div className="mt-16 basis-1/2 md:mt-0">
           <img alt="logo" src={Logo} />
           <p className="my-5">
-            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
-            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
-            orci ut habitant laoreet. Iaculis tristique.
+            At our gym, we believe in the power of fitness to transform lives.
+            We offer a wide range of equipment and classes to suit every fitness
+            level and goal, whether you're just starting out or are a seasoned
+            athlete. Our certified trainers are dedicated to helping you achieve
+            your best self through personalized training and nutrition plans.
           </p>
-          <p>© Evogym All Rights Reserved.</p>
+          <p>© Farouk'Gym All Rights Reserved 2023.</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
-          <p className="my-5">Massa orci senectus</p>
-          <p className="my-5">Et gravida id et etiam</p>
-          <p>Ullamcorper vivamus</p>
+          <a className="my-5" href="https://faroukallani.tn">farouk allani Portfolio</a> <br />
+          <a className="my-5" href="https://lostandfound.faroukallani.tn/">Lost and Found App</a> <br />
+          <a className="my-5" href="https://olc.faroukallani.tn/">Open Learning Community Platform</a> <br />
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+          <p className="my-5">Tunisia, Tunis 5000, Street Abc</p>
           <p>+216 23 550 577</p>
         </div>
       </div>
